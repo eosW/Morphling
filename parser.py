1964ef487c19338c7368cc37d7bccd504f027870
endofsamsara <end.wrm@gmail.com>
2017-02-28 20:41:11 -0800

    AST spec

--- parser.py
+++ parser.py
@@ -1,3 +1,4 @@
+import sys,json
 from ply import lex,yacc
 
 reserved = {
@@ -13,11 +14,13 @@ reserved = {
    'bool' : 'TYPE',
    'false' : 'BOOL',
    'true' : 'BOOL',
+   'read' : 'READ',
+   'write' : 'WRITE',
 }
 
 tokens = ['NAME','TYPE','INT','FLOAT','BOOL','PLUS','MINUS','TIMES','DIVIDES','MODS','EXP','BRSHIFT','BLSHIFT','BXOR'
             ,'BOR','BAND','BNOT','OR','AND','NOT','EQ','NEQ','GT','GET','LT','LET','LP','RP','COLON','SEMICOLON'
-            ,'ASSIGN','END','WHILE','IF','ELIF','ELSE','BREAK','CONTINUE']
+            ,'ASSIGN','END','WHILE','IF','ELIF','ELSE','BREAK','CONTINUE','READ','WRITE']
 
 def t_NAME(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
@@ -90,6 +93,8 @@ def p_sentence(p):
     sentence : declare SEMICOLON
     | assign SEMICOLON
     | control SEMICOLON
+    | read SEMICOLON
+    | write SEMICOLON
     """
     p[0] = p[1]
 
@@ -97,19 +102,19 @@ def p_declare(p):
     """
     declare : TYPE NAME
     """
-    p[0] = ('declare', p[1], p[2])
+    p[0] = {'sentence':'declare', 'type':p[1], 'name':p[2]}
 
 def p_declare2(p):
     """
     declare : TYPE NAME ASSIGN expr
     """
-    p[0] = ('declare_init', p[1], p[2], p[4])
+    p[0] = {'sentence':'declare_init', 'type':p[1], 'name':p[2], 'expr':p[4]}
 
 def p_assign(p):
     """
     assign : NAME ASSIGN expr
     """
-    p[0] = ('assign', p[1], p[3])
+    p[0] = {'sentence':'assign', 'type':p[1], 'expr':p[3]}
 
 def p_control(p):
     """
@@ -123,25 +128,25 @@ def p_control2(p):
     control : BREAK
     | CONTINUE
     """
-    p[0] = (p[1],)
+    p[0] = {'sentence':p[1]}
 
 def p_while(p):
     """
     while : WHILE LP expr RP COLON block END
     """
-    p[0] = ('while', p[3], p[6])
+    p[0] = {'sentence':'while', 'expr':p[3], 'block':p[6]}
 
 def p_if(p):
     """
     if : IF LP expr RP COLON block elif else END
     """
-    p[0] = ('if', [(p[3], p[6])] + p[7] + p[8])
+    p[0] = {'sentence':'if', 'cases':[{'condition':p[3], 'block':p[6]}] + p[7] + p[8]}
 
 def p_elif(p):
     """
     elif : elif ELIF LP expr RP COLON block
     """
-    p[0] = p[1] + [(p[4], p[7])]
+    p[0] = p[1] + [{'condition':p[4], 'block':p[7]}]
 
 def p_elif2(p):
     """
@@ -153,7 +158,7 @@ def p_else(p):
     """
     else : ELSE COLON block
     """
-    p[0] = [('true', p[3])]
+    p[0] = [{'condition':{'type':'BOOL','value':'true'}, 'block':p[3]}]
 
 def p_else2(p):
     """
@@ -176,14 +181,14 @@ def p_aexpr_b(p):
     | aexpr DIVIDES aexpr
     | aexpr EXP aexpr
     """
-    p[0] = (p[2], p[1], p[3])
+    p[0] = {'operator':p[2], 'e1':p[1], 'e2':p[3]}
 
 def p_aexpr_u(p):
     """
     aexpr : MINUS aexpr %prec NEGATIVE
     | BNOT aexpr
     """
-    p[0] = (p[1], p[2])
+    p[0] = {'operator':p[1], 'e1':p[2]}
 
 def p_aexpr_p(p):
     """
@@ -195,7 +200,7 @@ def p_aexpr_cast(p):
     """
     aexpr : TYPE LP aexpr RP
     """
-    p[0] = ('cast', p[1], p[3])
+    p[0] = {'operator': 'cast', 'e1': p[1], 'e2':p[3]}
 
 def p_aexpr_n(p):
     """
@@ -204,7 +209,7 @@ def p_aexpr_n(p):
     | BOOL
     | NAME
     """
-    p[0] = (p.slice[1].type,p[1])
+    p[0] = {'type':p.slice[1].type,'value':p[1]}
 
 def p_expr_b(p):
     """
@@ -217,13 +222,13 @@ def p_expr_b(p):
     | aexpr LT aexpr
     | aexpr LET aexpr
     """
-    p[0] = (p[2], p[1], p[3])
+    p[0] = {'operator':p[2], 'e1':p[1], 'e2':p[3]}
 
 def p_expr_u(p):
     """
     expr : NOT expr
     """
-    p[0] = (p[1], p[2])
+    p[0] = {'operator':p[1], 'e1':p[2]}
 
 def p_expr_dg(p):
     """
@@ -231,8 +236,17 @@ def p_expr_dg(p):
     """
     p[0] = p[1]
 
-def p_error(p):
-    pass
+def p_read(p):
+    """
+    read : READ NAME
+    """
+    p[0] = {'sentence':'read', 'name':p[2]}
+
+def p_write(p):
+    """
+    write : WRITE expr
+    """
+    p[0] = {'sentence':'write', 'expr':p[2]}
 
 precedence = (
     ('left', 'OR'),
@@ -249,22 +263,39 @@ precedence = (
     ('left', 'EXP'),
 )
 
-# import logging
-# logging.basicConfig(
-#     level = logging.DEBUG,
-#     filename = "parselog.txt",
-#     filemode = "w",
-#     format = "%(filename)10s:%(lineno)4d:%(message)s"
-# )
-# log = logging.getLogger()
+import logging
+logging.basicConfig(
+    level = logging.DEBUG,
+    filename = "parselog.txt",
+    filemode = "w",
+    format = "%(filename)10s:%(lineno)4d:%(message)s"
+)
+log = logging.getLogger()
 
 parser = yacc.yacc()
-data = "float a; a = 4e4*7+-.5**7; int b = 5^7<<4;"
-print data
-print parser.parse(data)
-data = "while (a>5): a = 7+9; b = false; break; end;"
-print data
-print parser.parse(data)
-data = "if (a>3): a=6; elif (a>6): a=7; elif (a<0): a=1; else: a=9; end;"
-print data
-print parser.parse(data)
+
+# data = "float a; a = 4e4*7+-.5**7; int b = 5^7<<4;"
+# print data
+# print parser.parse(data)
+# data = "while (a>5): a = 7+9; b = false; break; end;"
+# print data
+# print parser.parse(data)
+# data = "if (a>3): a=6; elif (a>6): a=7; elif (a<0): a=1; else: a=9; end;"
+# print data
+# print parser.parse(data)
+# data = "read a; write a+6*b;"
+# print data
+# print parser.parse(data)
+
+if __name__ == "__main__":
+    args = sys.argv
+    filein = args[1]
+    segs = filein.split('.')
+    if len(segs)>1:
+        fileout = '.'.join(segs[:-1])+'.ast'
+    else:
+        fileout = filein+'.ast'
+    with open(filein) as source, open(fileout,'wb') as target:
+        text = source.read()
+        ast = parser.parse(text)
+        json.dump(ast,target,sort_keys=True, indent=2, separators=(',', ': '))
