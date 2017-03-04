51fb342362e2f2d97500bd87bebae116cf2ccf50
endofsamsara <end.wrm@gmail.com>
2017-03-03 18:41:23 -0800

    Type checker

--- parser.py
+++ parser.py
@@ -76,6 +76,8 @@ def t_error(t):
 
 lex.lex()
 
+typetable = {}
+
 def p_block(p):
     """
     block : block sentence
@@ -102,19 +104,31 @@ def p_declare(p):
     """
     declare : TYPE NAME
     """
+    typetable[p[2]] = p[1]
     p[0] = {'sentence':'declare', 'type':p[1], 'name':p[2]}
 
 def p_declare2(p):
     """
     declare : TYPE NAME ASSIGN expr
     """
-    p[0] = {'sentence':'declare_init', 'type':p[1], 'name':p[2], 'expr':p[4]}
+    if p[4]['type'] != p[1]:
+        print("ERROR: line{:d}: type mismatch".format(p.slice[1].lineno))
+        raise SyntaxError
+    typetable[p[2]] = p[1]
+    p[0] = {'sentence':'declare_init', 'type':p[1], 'name':p[2], 'expr':p[4]['value']}
 
 def p_assign(p):
     """
     assign : NAME ASSIGN expr
     """
-    p[0] = {'sentence':'assign', 'type':p[1], 'expr':p[3]}
+    if p[1] not in typetable:
+        print("ERROR: line{:d}: undecleared variable {:s}".format(p.slice[1].lineno,p[1]))
+        raise SyntaxError
+    if p[3]['type'] != typetable[p[1]]:
+        print("ERROR: line{:d}: type mismatch".format(p.slice[1].lineno))
+        raise SyntaxError
+    typetable[p[2]] = p[1]
+    p[0] = {'sentence':'assign', 'type':p[1], 'expr':p[3]['value']}
 
 def p_control(p):
     """
@@ -134,19 +148,28 @@ def p_while(p):
     """
     while : WHILE LP expr RP COLON block END
     """
-    p[0] = {'sentence':'while', 'expr':p[3], 'block':p[6]}
+    if p[3]['type'] != 'bool':
+        print("ERROR: line{:d}: expression is not bool".format(p.slice[3].lineno))
+        raise SyntaxError
+    p[0] = {'sentence':'while', 'expr':p[3]['value'], 'block':p[6]}
 
 def p_if(p):
     """
     if : IF LP expr RP COLON block elif else END
     """
-    p[0] = {'sentence':'if', 'cases':[{'condition':p[3], 'block':p[6]}] + p[7] + p[8]}
+    if p[3]['type'] != 'bool':
+        print("ERROR: line{:d}: expression is not bool".format(p.slice[3].lineno))
+        raise SyntaxError
+    p[0] = {'sentence':'if', 'cases':[{'condition':p[3]['value'], 'block':p[6]}] + p[7] + p[8]}
 
 def p_elif(p):
     """
     elif : elif ELIF LP expr RP COLON block
     """
-    p[0] = p[1] + [{'condition':p[4], 'block':p[7]}]
+    if p[4]['type'] != 'bool':
+        print("ERROR: line{:d}: expression is not bool".format(p.slice[4].lineno))
+        raise SyntaxError
+    p[0] = p[1] + [{'condition':p[4]['value'], 'block':p[7]}]
 
 def p_elif2(p):
     """
@@ -167,28 +190,55 @@ def p_else2(p):
     p[0] = []
 
 
-def p_aexpr_b(p):
+def p_aexpr_b_b(p):
     """
     aexpr : aexpr BOR aexpr
     | aexpr BXOR aexpr
     | aexpr BAND aexpr
     | aexpr BRSHIFT aexpr
     | aexpr BLSHIFT aexpr
-    | aexpr PLUS aexpr
+    """
+    if p[1]['type'] != 'int' or p[3]['type'] != 'int':
+        print("ERROR: line{:d}: operator {:s} applies to int values only".format(p.slice[2].lineno,p[2]))
+        raise SyntaxError
+    p[0] = {'value':{'operator':p[2], 'e1':p[1]['value'], 'e2':p[3]['value']},'type':'int'}
+
+
+def p_aexpr_b_n(p):
+    """
+    aexpr : aexpr PLUS aexpr
     | aexpr MINUS aexpr
     | aexpr TIMES aexpr
     | aexpr MODS aexpr
     | aexpr DIVIDES aexpr
     | aexpr EXP aexpr
     """
-    p[0] = {'operator':p[2], 'e1':p[1], 'e2':p[3]}
+    if p[1]['type'] == 'bool' or p[3]['type'] == 'bool':
+        print("ERROR: line{:d}: operator {:s} does not applies to bool values".format(p.slice[2].lineno,p[2]))
+        raise SyntaxError
+    if p[1]['type'] == 'int' and p[3]['type'] == 'int':
+        restype = 'int'
+    else:
+        restype = 'float'
+    p[0] = {'value':{'operator':p[2], 'e1':p[1]['value'], 'e2':p[3]['value']},'type':restype}
 
-def p_aexpr_u(p):
+def p_aexpr_u_n(p):
     """
     aexpr : MINUS aexpr %prec NEGATIVE
-    | BNOT aexpr
     """
-    p[0] = {'operator':p[1], 'e1':p[2]}
+    if p[2]['type'] == 'bool':
+        print("ERROR: line{:d}: operator {:s} does not applies to bool values".format(p.slice[1].lineno,p[1]))
+        raise SyntaxError
+    p[0] = {'value':{'operator':p[1], 'e1':p[2]['value']},'type':p[2]['type']}
+
+def p_aexpr_u_b(p):
+    """
+    aexpr : BNOT aexpr
+    """
+    if p[2]['type'] != 'bool':
+        print("ERROR: line{:d}: operator {:s} applies to bool values only".format(p.slice[1].lineno,p[1]))
+        raise SyntaxError
+    p[0] = {'value':{'operator':p[1], 'e1':p[2]['value']},'type':p[2]['type']}
 
 def p_aexpr_p(p):
     """
@@ -200,7 +250,7 @@ def p_aexpr_cast(p):
     """
     aexpr : TYPE LP aexpr RP
     """
-    p[0] = {'operator': 'cast', 'e1': p[1], 'e2':p[3]}
+    p[0] = {'value':{'operator': 'cast', 'e1': p[1], 'e2':p[3]['value']},'type':p[1]}
 
 def p_aexpr_n(p):
     """
@@ -209,26 +259,46 @@ def p_aexpr_n(p):
     | BOOL
     | NAME
     """
-    p[0] = {'type':p.slice[1].type,'value':p[1]}
+    restype = p.slice[1].type.lower()
+    if restype == 'name':
+        if p[1] not in typetable:
+            print("ERROR: line{:d}: undecleared variable {:s}".format(p.slice[1].lineno,p[1]))
+            raise SyntaxError
+        restype = typetable[p[1]]
+    p[0] = {'value':{'type':p.slice[1].type,'value':p[1]},'type':restype}
 
-def p_expr_b(p):
+def p_expr_b_b(p):
     """
     expr : expr OR expr
     | expr AND expr
-    | aexpr EQ aexpr
+    """
+    if p[1]['type'] != 'bool' or p[3]['type'] != 'bool':
+        print("ERROR: line{:d}: operator {:s} applies to bool values only".format(p.slice[2].lineno,p[2]))
+        raise SyntaxError
+    p[0] = {'value':{'operator':p[2], 'e1':p[1]['value'], 'e2':p[3]['value']},'type':'bool'}
+
+def p_expr_c(p):
+    """
+    expr :  aexpr EQ aexpr
     | aexpr NEQ aexpr
     | aexpr GT aexpr
     | aexpr GET aexpr
     | aexpr LT aexpr
     | aexpr LET aexpr
     """
-    p[0] = {'operator':p[2], 'e1':p[1], 'e2':p[3]}
+    if p[1]['type'] == 'bool' or p[3]['type'] == 'bool':
+        print("ERROR: line{:d}: operator {:s} does not applies to bool values".format(p.slice[2].lineno,p[2]))
+        raise SyntaxError
+    p[0] = {'value':{'operator':p[2], 'e1':p[1]['value'], 'e2':p[3]['value']},'type':'bool'}
 
 def p_expr_u(p):
     """
     expr : NOT expr
     """
-    p[0] = {'operator':p[1], 'e1':p[2]}
+    if p[2]['type'] != 'bool':
+        print("ERROR: line{:d}: operator {:s} applies to bool values only".format(p.slice[1].lineno,p[1]))
+        raise SyntaxError
+    p[0] = {'value':{'operator':p[1], 'e1':p[2]['value']},'type':p[2]['type']}
 
 def p_expr_dg(p):
     """
@@ -246,7 +316,7 @@ def p_write(p):
     """
     write : WRITE expr
     """
-    p[0] = {'sentence':'write', 'expr':p[2]}
+    p[0] = {'sentence':'write', 'expr':p[2]['value']}
 
 precedence = (
     ('left', 'OR'),
@@ -274,19 +344,6 @@ log = logging.getLogger()
 
 parser = yacc.yacc()
 
-# data = "float a; a = 4e4*7+-.5**7; int b = 5^7<<4;"
-# print data
-# print parser.parse(data)
-# data = "while (a>5): a = 7+9; b = false; break; end;"
-# print data
-# print parser.parse(data)
-# data = "if (a>3): a=6; elif (a>6): a=7; elif (a<0): a=1; else: a=9; end;"
-# print data
-# print parser.parse(data)
-# data = "read a; write a+6*b;"
-# print data
-# print parser.parse(data)
-
 if __name__ == "__main__":
     args = sys.argv
     filein = args[1]
@@ -297,5 +354,6 @@ if __name__ == "__main__":
         fileout = filein+'.ast'
     with open(filein) as source, open(fileout,'wb') as target:
         text = source.read()
-        ast = parser.parse(text)
-        json.dump(ast,target,sort_keys=True, indent=2, separators=(',', ': '))
+        ast = parser.parse(text,debug=log)
+        if parser.errorok:
+            json.dump(ast,target,sort_keys=True, indent=2, separators=(',', ': '))
