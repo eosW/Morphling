50bcf57540446168f3348676dab93756a7a8ad02
endofsamsara <end.wrm@gmail.com>
2017-03-09 17:32:47 -0800

    add feature

--- java_gen.py
+++ java_gen.py
@@ -26,7 +26,7 @@ def genblock(block,target,ind):
             expr = sentence['expr']
             target.write("{:s}{:s} {:s} = {:s};\n".format(indstr,{'int':'int','float':'double','bool':'boolean'}[dtype],name,expreval(expr)))
         if stype == "assign":
-            name = sentence['type']
+            name = sentence['name']
             expr = sentence['expr']
             target.write("{:s}{:s} = {:s};\n".format(indstr,name,expreval(expr)))
         if stype == "break":
